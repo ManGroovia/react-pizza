0018e19e12825eb9281da43ad3add6b163ba3994
ManGroovia <127965606+ManGroovia@users.noreply.github.com>
2023-04-30 18:48:42 +0600

    Added first respository

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Header.jsx";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title='Мексиканская' price={500}/>
+            <PizzaBlock title='Маргарита' price={400} />
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
